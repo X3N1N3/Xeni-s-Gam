d782f0140e6bfb81606cab44845c5c3ab0a0689f
X3N1N3 <74053728+X3N1N3@users.noreply.github.com>
2021-02-28 16:57:16 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var godzillaImg, xenoImg, soldierImg, alienImg, heartImg, backGroundImg1, backGroundImg2
+
+
+
+
+
+
+function preload() {
+    heartImg = loadImage("Images/download (1).jpeg")
+    xenoImg = loadImage("Images/ajajjaja.png")
+soldierImg = loadImage("Images/Soldow.jpg")
+alienImg = loadImage("Images/AWIEN.png")
+backGroundImg1 = loadImage("Images/WhyIsThereANuclearPowerplantNearCivilisation.jpg")
+backGroundImg2 = loadImage("Images/WhyIsTheGroundCracked.jpg")
+
+}
